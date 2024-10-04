ff226c4978099392484632c8a4b1dffd9173209a
Ramletha1 <146055491+Ramletha1@users.noreply.github.com>
2024-10-04 03:41:48 +0000

    /home/codespace

--- System_Programming/low_level_file_access/charcopy/char_copy.c
+++ System_Programming/low_level_file_access/charcopy/char_copy.c
@@ -8,13 +8,13 @@ char c;
 int size;
 int in;
 int out;
-in = open("file2.in", O_RDONLY);
-out = open("file2.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+in = open("file.in", O_RDONLY);
+out = open("file.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
 while (read(in, &c, 1) == 1){
   write(out, &c, 1);
   size++;
 }
-  
+
   lseek(out,size/2,SEEK_SET);
   write(out,"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",16);
   lseek(in,size/2,SEEK_SET);
@@ -22,7 +22,6 @@ while (read(in, &c, 1) == 1){
   while (read(in, &c, 1) == 1){
     write(out, &c, 1);
   }
-  
 
 return 0;
 }
