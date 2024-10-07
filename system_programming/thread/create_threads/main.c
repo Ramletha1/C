07e6ffa734b872c287f95139142e5bf0fc06856f
Ramletha1 <146055491+Ramletha1@users.noreply.github.com>
2024-10-07 08:47:04 +0000

    .

--- system_programming/thread/create_threads/main.c
+++ system_programming/thread/create_threads/main.c
@@ -26,15 +26,19 @@ void *print_Ys(void *unused){
 
 int main(){
     int n;
-    pthread_t thread_id;
+    pthread_t thread_id1, thread_id2;
 
-    pthread_create(&thread_id, NULL, &print_Xs, NULL);
-    pthread_create(&thread_id, NULL, &print_Ys, NULL);
+    pthread_create(&thread_id1, NULL, &print_Xs, NULL);
+    pthread_create(&thread_id2, NULL, &print_Ys, NULL);
 
     for(n=100; n>0; n--){
         fputc('Z',stderr);
         usleep(1);
     }
-    usleep(1000000);
+    // usleep(100000);
+
+    pthread_join(thread_id1, NULL);   // NULL is until returned NULL
+    pthread_join(thread_id2, NULL);
+
     return 0;
 }
