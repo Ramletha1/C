13be4e1dd9e5a3d0f87e247f74b44584d6c2db47
Ramletha1 <146055491+Ramletha1@users.noreply.github.com>
2024-10-12 19:29:11 +0000

    /home/codespace

--- system_programming/thread/multi_thread_assignment/main.c
+++ system_programming/thread/multi_thread_assignment/main.c
@@ -1,53 +1,89 @@
 #include <pthread.h>
 #include <stdio.h>
-//Compute successive prime numbers (very inef ciently).
-//Return the Nth prime number, where N is the value pointed to by *ARG.v
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX_NUMBER 50000
-#define MAX_THREAD 5
+#define NUMBER 50000
+#define TOTAL_THREAD 5
+#define PRINT_ALL true
 
 typedef struct {
     int start;
     int end;
-    int* primes;
-    int* count;
+    int* local_primes;
+    int local_count;
 } ThreadArgs;
+
+// pthread_mutex_t shared_resource;
 
 void* compute_prime (void* arg) {
     ThreadArgs* args = (ThreadArgs*) arg;
     int start = args->start;
     int end = args->end;
-    int* primes = args->primes;
-    int* count = args->count;
+    int* primes = args->local_primes;
+    int count = 0;
 
-    for (int candidate = start; candidate<end; candidate++) {
+    for (int candidate = start; candidate<=end; candidate++) {
         if (candidate<2) continue;
-        //.......
+        bool is_prime = true;       // is prime
+
+        for (int i = 2; i * i <= candidate; i++) {
+            if (candidate % i == 0) {
+                is_prime = false;   // not prime
+                break;
+            }
+        }
+
+        if (is_prime) {
+            primes[count++] = candidate;
+        }
     }
+    args->local_count = count;
+    return NULL;
 }
 
 int main () {
-    pthread_t thread[MAX_THREAD];
-    ThreadArgs thread_args[MAX_THREAD];
-    int* primes = malloc(sizeof(int) * MAX_NUMBER);
-    int prime_count = 0;
+    pthread_t thread[TOTAL_THREAD];
+    ThreadArgs thread_args[TOTAL_THREAD];
+    int* primes = malloc(sizeof(int) * NUMBER);
 
-    for(int count=0; count<MAX_THREAD; count++) {
-        thread_args[i].start = (MAX_NUMBER/MAX_THREAD) * count;
-        thread_args[i].end = (MAX_NUMBER/MAX_THREAD) + thread_args[i].start;
-        thread_args[i].primes = primes;
-        thread_args[i].count = &prime_count;
-        pthread_create(&thread[i], NULL, compute_prime, thread_args[i]);
+    // pthread_mutex_init(&shared_resource, NULL);
+
+    for (int i=0; i<TOTAL_THREAD; i++) {
+        thread_args[i].start = (NUMBER/TOTAL_THREAD) * i;
+        thread_args[i].local_primes = malloc(sizeof(int) * (NUMBER/TOTAL_THREAD));
+        thread_args[i].local_count = 0;
+        if (i == TOTAL_THREAD - 1) { thread_args[i].end = NUMBER; }
+        else { thread_args[i].end = (NUMBER/TOTAL_THREAD) * (i + 1) - 1; }
+
+        pthread_create(&thread[i], NULL, compute_prime, &thread_args[i]);
     }
 
-    for(int count=0; count<MAX_THREAD; count++) {
-        pthread_join(&thread[i], NULL);
+    for (int i=0; i<TOTAL_THREAD; i++) {
+        pthread_join(thread[i], NULL);
+    }
+
+    int prime_count = 0;
+    for (int i=0; i<TOTAL_THREAD; i++) {
+        // pthread_lock(&shared_resource);
+        for (int j=0; j < thread_args[i].local_count; j++) {
+            primes[prime_count++] = thread_args[i].local_primes[j];
+        }
+        // pthread_unlock(&shared_resoruce);
+        free(thread_args[i].local_primes);
     }
     
-    printf("Printing all prime number from 0 to %d.\n", MAX_NUMBER);
-    for(int count=0; count<prime_count; count++) {
-        printf("%d\n", primes[count]);
+    if (PRINT_ALL) {
+        for (int i=0; i<prime_count; i++) { printf(" %d", primes[i]); }
     }
+    else {
+        for (int i=0; i<5; i++) { printf(" %d", primes[i]); }
+        printf(" ...");
+        for (int i=prime_count-5; i<prime_count; i++) { printf(" %d", primes[i]); }
+    }
+    printf("\nTotal prime count is %d.\n", prime_count);
 
+    // pthread_mutex_destroy(&shared_resource);
+    free(primes);   // free allocated memory
     return 0;
 }
